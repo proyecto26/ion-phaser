38c96969530f39f7ede2057a955f9559d9f1f294
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2019-09-02 23:03:38 -0500

    Fix React wrapper configuration

--- react/rollup.config.js
+++ react/rollup.config.js
@@ -1,15 +1,19 @@
 import resolve from 'rollup-plugin-node-resolve';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
 export default {
   input: 'dist-transpiled/index.js',
   output: [
     {
       file: 'dist/index.esm.js',
-      format: 'es'
+      format: 'es',
+      sourcemap: true
     },
     {
       file: 'dist/index.js',
-      format: 'commonjs'
+      format: 'commonjs',
+      preferConst: true,
+      sourcemap: true
     }
   ],
   external: [
@@ -20,6 +24,7 @@ export default {
     'phaser'
   ],
   plugins: [
-    resolve()
+    resolve(),
+    sourcemaps()
   ]
 };
